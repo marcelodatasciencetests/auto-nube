332aad43883e8f633955d64a11a04b0346fb3e17
marcelodatasciencetests <datasciencetests12@gmail.com>
2025-02-07 13:05:35 -0400

    Se agrego el checkbox

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ import plotly_express as px
 st.header("Datos de vehiculos -- Concencionario base de datos")
 datos_autos = pd.read_csv("vehicles_us.csv")
 
+#opciones de los usuarios
+
+grafico_histograma = st.checkbox("Generar histograma")
+grafico_dispersion = st.checkbox("Generar grafico de dispersion")
+
 # Crear el boton que genera el histograma. 
 
-if st.button("Generar histograma") :
+if grafico_histograma:
+    fig = px.histogram(datos_autos, x='odometer', title='Distribución del kilometraje')
+    fig.update_layout(bargap = 0.2) 
+    st.plotly_chart(fig)   
     
-    fig = px.histogram(datos_autos, x='odometer', title='Distribucion del kilometraje')       
-    fig.update_layout(bargap = 0.2)         
-    st.plotly_chart(fig)     
     
-    
-if st.button("Generar grafico de dispersion") :
+if grafico_dispersion:
     
     fig = px.scatter(datos_autos, x='odometer', title='Distribucion del kilometraje')       
     fig.update_layout(bargap = 0.2)         
